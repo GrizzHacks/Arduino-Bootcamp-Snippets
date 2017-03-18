5a49ceaff210f30cfed4628c78377510d5968e0a
AdamMc331 <amcneilly331@gmail.com>
2017-03-18 09:27:59 -0400

    Adding blink snippet.

---
+++ blink.ino
@@ -0,0 +1,15 @@
+// Set pin
+int led = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
